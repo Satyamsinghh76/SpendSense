239b10ac5a4309bd0a3dd333529c58911bca7156
satyamsinghlko48@gmail.com <satyamsinghlko48@gmail.com>
2026-04-27 14:26:42 +0530

    test: add simple GET endpoint to debug HTTP routing

--- convex/http.ts
+++ convex/http.ts
@@ -1,9 +1,20 @@
 import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
 import { auth } from "./auth";
 
+// Create router and add auth routes
 const http = httpRouter();
-
-// Add auth routes
 auth.addHttpRoutes(http);
+
+// Try adding a test HTTP route  
+http.route({
+  path: "/api/test",
+  method: "GET",
+  handler: httpAction(async (ctx, request) => {
+    return new Response(JSON.stringify({ message: "Test works!" }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }),
+});
 
 export default http;
