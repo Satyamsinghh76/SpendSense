5362abf9143b30846aa94bdf6940e809060c45a5
satyamsinghlko48@gmail.com <satyamsinghlko48@gmail.com>
2026-04-27 14:23:30 +0530

    fix: reuse router instance from router.ts

--- convex/http.ts
+++ convex/http.ts
@@ -1,8 +1,6 @@
-import { httpRouter } from "convex/server";
+import http from "./router";
 import { httpAction } from "./_generated/server";
 import { auth } from "./auth";
-
-const http = httpRouter();
 
 // Add auth routes first
 auth.addHttpRoutes(http);
